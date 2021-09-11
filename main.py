9a9f0afcea3cef067b915ff2edd94b680c3e6c82
NOP <57258174+mplein2@users.noreply.github.com>
2021-09-11 16:36:04 +0300

    Homepage Removed Extra Stuff.

--- main.py
+++ main.py
@@ -14,12 +14,7 @@ log.disabled = True
 
 @app.route("/")
 def main():
-    return render_template("index.html", ip=c1.publicIP)
-
-
-@app.route("/blank")
-def blank():
-    return render_template("blank.html")
+    return render_template("dashboard.html", ip=c1.publicIP)
 
 
 if __name__ == "__main__":
