caea14a17766b539aa57d9a4d672613e0539e9ee
NOP <57258174+mplein2@users.noreply.github.com>
2022-01-26 11:01:42 +0200

    Start

---
+++ main.py
@@ -0,0 +1,18 @@
+import logging
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+# This Disables Logging
+app.logger.disabled = False
+log = logging.getLogger('werkzeug')
+log.disabled = False
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=6969)
