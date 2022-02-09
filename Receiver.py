e8f2338fd9f2cd73be04c2ca7d404640cd7e28cd
NOP <57258174+mplein2@users.noreply.github.com>
2022-02-09 09:47:09 +0200

    test

--- Receiver.py
+++ Receiver.py
@@ -9,19 +9,14 @@ import struct
 
 
 def receiver():
-    port = 6700
-    # Create the socket
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-    # Set some options to make it multicast-friendly
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_TTL, 32)
-    s.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_LOOP, 2)
-    s.bind(('', port))
-
-    # Set some more multicast options
-    intf = socket.gethostbyname(socket.gethostname())
-    s.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_IF, socket.inet_aton(intf))
+    import socket
+    import struct
+    MCAST_GRP = '224.1.1.1'
+    MCAST_PORT = 5004
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(('', MCAST_PORT))
+    mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
     while True:
-        print("Working")
-        print(s.recvfrom(10000))
+        print(sock.recv(10240))
--- send.py
+++ send.py
@@ -1,15 +1,22 @@
 import socket
 import sys
 
-addr = '82.102.57.157'
+addr = '244.1.1.1'
 port = 6700
 data = "yolo".encode()
 
 if __name__ == "__main__":
-    # Create the socket
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    # Make the socket multicast-aware, and set TTL.
-    s.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)  # Change TTL (=20) to suit
-    # Send the data
-    s.sendto(data, (addr, port))
+    import socket
+
+    group = '224.1.1.1'
+    port = 5004
+    # 2-hop restriction in network
+    ttl = 2
+    sock = socket.socket(socket.AF_INET,
+                         socket.SOCK_DGRAM,
+                         socket.IPPROTO_UDP)
+    sock.setsockopt(socket.IPPROTO_IP,
+                    socket.IP_MULTICAST_TTL,
+                    ttl)
+    sock.sendto(b"hello world", (group, port))
 
