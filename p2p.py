6a0387ef3b160f9d65db10b4075423fd763a84dd
NOP <57258174+mplein2@users.noreply.github.com>
2022-02-09 17:33:48 +0200

    Test1

--- p2p.py
+++ p2p.py
@@ -1,37 +1,35 @@
 import socket
-
-
-class ConnectionManager:
-    def __init__(self):
-        pass
-
+import threading
+import time
 
 def receiver():
     while True:
         # Get UDP From here.
-        UDP_IP = "127.0.0.1"
         UDP_PORT = 6700
         sock = socket.socket(socket.AF_INET,  # Internet
                              socket.SOCK_DGRAM)  # UDP
-        sock.bind((UDP_IP, UDP_PORT))
+        sock.bind(("", UDP_PORT))
+        y = threading.Thread(target=keepAlive,args=[sock])
+        y.start()
         while True:
+            print("Trying to receive")
             data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
             print("Received:", data, " from", addr)
 
 
 # peers should be a list
-def keepAlive(peers):
-    peers = ["82.1.1.1", ]
-    UDP_IP = "127.0.0.1"
-    UDP_PORT = 6700
+def keepAlive(sock):
+    peers = ["82.102.57.157", ]
     while True:
+        time.sleep(5)
         for peer in peers:
-            sock = socket.socket(socket.AF_INET,  # Internet
-                                 socket.SOCK_DGRAM)  # UDP
-            sock.bind((UDP_IP, UDP_PORT))
-            sock.sendto(b'KeepAlive', peer)
-    sock.sendto(b'Data', peer)
+            print("Sending KeepAlive to ",peer,":",6700)
+            sock.sendto(b'KeepAlive', (peer,6700))
+        sock.sendto(b'Data', (peer,6700))
 
 
 if __name__ == "__main__":
-    keepAlive()
+    x = threading.Thread(target=receiver)
+    x.start()
+
+
