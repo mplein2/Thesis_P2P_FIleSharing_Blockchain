3eb73fde4eb74b904931a5cfea793970fd8a4459
Ftoy <57258174+mplein2@users.noreply.github.com>
2022-03-28 19:17:39 +0300

    Laptop Fix typo and tuple on receiver from prev comit

--- Networking.py
+++ Networking.py
@@ -7,22 +7,25 @@ from Groups import Group
 
 
 class Request:
-    def __init__(self,type):
+    def __init__(self, type):
         self.type = type
 
     def toJSON(self):
         return json.dumps(self.__dict__)
 
+
 class JoinRequest(Request):
-    def __init__(self,name,timestamp):
+    def __init__(self, name, timestamp):
         super().__init__(1)
         self.name = name
         self.timestamp = timestamp
 
+
 class JoinResponse(Request):
-    def __init__(self,group):
+    def __init__(self, group):
         super().__init__(1)
         self.group = group
+
 
 class SearchBundleRequest(Request):
     def __init__(self, groupId, keyword):
@@ -30,19 +33,21 @@ class SearchBundleRequest(Request):
         self.groupID = groupId
         self.keywords = keyword
 
+
 class SearchBundleResponse(Request):
-    def __init__(self,groups):
+    def __init__(self, responseBundles):
         super().__init__(2)
-        self.responseBundles = groups
+        self.responseBundles = responseBundles
+
 
 def requestHandler(data, addr, groupManager: Groups.GroupManager):
     req = pickle.loads(data)
     # Response to JoinRequest
     if req.type == 1:
-        req = JoinRequest(req.name,req.timestamp)
-        #TODO If this is allowed add peer and respond
-        #TODO Peer Port
-        groupManager.addPeerGroup(req.name,[addr[0]])
+        req = JoinRequest(req.name, req.timestamp)
+        # TODO If this is allowed add peer and respond
+        # TODO Peer Port
+        groupManager.addPeerGroup(req.name, [addr[0]])
         group = groupManager.getGroup(req.name)
         groupCpy = copy.copy(group)
         del groupCpy.bundles
@@ -50,22 +55,23 @@ def requestHandler(data, addr, groupManager: Groups.GroupManager):
         return pickle.dumps(joinResponse)
 
     elif req.type == 2:
-        #Create The Request again for local use
-        req = SearchBundleRequest(req.groupID,req.keywords)
-        #Get All Bundles User has
+        # Create The Request again for local use
+        req = SearchBundleRequest(req.groupID, req.keywords)
+        # Get All Bundles User has
         bundlesOfGroup = groupManager.getGroupWithID(req.groupID).bundles
-        #Bundles to reply
+        # Bundles to reply
         responseBundles = []
         for bundle in bundlesOfGroup:
-            #For each bundle make a single list (bundleKeywords) and search if any of the keywords other user send are in there then send back
+            # For each bundle make a single list (bundleKeywords) and search if any of the keywords other user send are in there then send back
             name = bundle.name.split()
             desc = bundle.description.split()
             bundleKeywords = name + desc
             if any(keyword in req.keywords for keyword in bundleKeywords):
-                responseBundles.append({"id":bundle.id,"name":bundle.name,"description":bundle.description})
-        #Create Response and return it to be used as answer
+                responseBundles.append({"id": bundle.id, "name": bundle.name, "description": bundle.description})
+        # Create Response and return it to be used as answer
         searchResponse = SearchBundleResponse(responseBundles)
-        return addr,pickle.dumps(searchResponse)
+        return pickle.dumps(searchResponse)
+
 
 def responseHandler(data, groupManager):
     res = pickle.loads(data)
@@ -78,6 +84,7 @@ def responseHandler(data, groupManager):
         res = SearchBundleResponse(res.responseBundles)
         return res
 
+
 def receiver(groupManager):
     UDP_IP = '0.0.0.0'
     UDP_PORT = 6700
@@ -85,27 +92,22 @@ def receiver(groupManager):
     sock.bind((UDP_IP, UDP_PORT))
     print("Listening on ", UDP_IP, ":", UDP_PORT)
     while True:
-        #RECEIVE AND RESPOND.
+        # RECEIVE AND RESPOND.
         data, addr = sock.recvfrom(65507)
-        print(addr,data)
-        response = requestHandler(data,addr,groupManager)
-        sock.sendto(response,addr)
+        print(addr, data)
+        response = requestHandler(data, addr, groupManager)
+        sock.sendto(response, addr)
 
-def sendRequest(address, port, request,groupManager):
+
+def sendRequest(address, port, request, groupManager):
     # Create a socket for sending files
     clientSocket = socket(AF_INET, SOCK_DGRAM)
     clientSocket.settimeout(1)
     try:
         clientSocket.sendto(request, (address, port))
         data, addr = clientSocket.recvfrom(65507)
-        res =  responseHandler(data, addr, groupManager)
-    #TODO except socket.timeout
-    #TODO for better exception handling fix later.
+        res = responseHandler(data, addr, groupManager)
+    # TODO except socket.timeout
+    # TODO for better exception handling fix later.
     except:
         return False
-
-
-
-
-
-
