ae247badae8c55d3e507e1fa37f4599871c48816
NOP <57258174+mplein2@users.noreply.github.com>
2022-03-31 20:18:40 +0300

    SEND/RECEIVE BUNDLE

--- Networking.py
+++ Networking.py
@@ -152,7 +152,7 @@ def sendRequest(address, port, request, groupManager):
 
 def receiveBundle(port):
     print("RECEIVING BUNDLE THREAD")
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    with socket(AF_INET, SOCK_STREAM) as s:
         s.bind(("0.0.0.0", port))
         print("Bundle Receiver Ready")
         s.listen()
@@ -167,7 +167,7 @@ def receiveBundle(port):
 
 def sendBundle(addr,port,bundle):
     print("SENDING BUNDLE THREAD")
-    with socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    with socket(AF_INET,SOCK_STREAM) as s:
         s.bind((addr, port))
         print("Bundle Receiver Ready")
         s.listen()
